ff88a090cea76429fcd9f536aab3d1e2a6c2ada9
jason.schein <jasonschein@gmail.com>
2014-04-24 15:07:40 -0700

    First test to see if autoloading is right.

---
+++ src/SecretSanta/Tests/AList/FileLoaderTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Echochamber\SecretSanta\Tests\AList;
+
+class FileLoaderTest extends \PHPUnit_Framework_TestCase
+{
+	public function testFirst()
+	{
+		
+		$this->assertTrue(true);
+	}
+}
